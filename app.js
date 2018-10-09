5284be3db33a4cae84949085032a4c7f7b9935c6
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-10-08 21:12:12 -0400

    Get stock info from api on click and log to console
    Render button elements for each stock symbol in stockList[]. Bind
    to click event listener that calls an XHR request for specific symbol
    that was clicked.
    
    XHR uses placeholder url; only gets quote info

---
+++ app.js
@@ -0,0 +1,31 @@
+const stockList = ['AAPL', 'TSLA', 'MSFT'];
+
+function getStockInfo(symbol) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://api.iextrading.com/1.0/stock/${symbol}/quote`);
+    xhr.onload = function() {
+        var data = JSON.parse(xhr.responseText);
+        console.log(data);
+    };
+    xhr.send();
+}
+
+const btnsDiv = document.querySelector('.stock-btns');
+function renderBtns() {
+    var btn;
+    for (const stock of stockList) {
+        btn = document.createElement('button');
+        btn.appendChild(
+            document.createTextNode(stock)
+        );
+        btnsDiv.appendChild(btn);
+    }
+}
+renderBtns();
+
+btnsDiv.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.matches('button')) {
+        getStockInfo(target.textContent);
+    }
+});
